23dd2a57331bf8814069728fa5613bedcef0ac81
murilo-scapim <mhscapim@gmail.com>
2024-09-04 20:19:26 -0300

    Criação da tela cursos

---
+++ src/br/com/fafram/view/CursoForm.java
@@ -0,0 +1,14 @@
+package br.com.fafram.view;
+
+import javax.swing.*;
+
+public class CursoForm {
+    private JPanel panel;
+    private JTextField textFieldId;
+    private JTextField textFieldNome;
+    private JTextField textFieldCarga;
+    private JButton cadastrarButton;
+    private JButton atualizarButton;
+    private JButton apagarButton;
+    private JTable tableCursos;
+}
