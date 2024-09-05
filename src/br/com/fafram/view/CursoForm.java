681dc6083f953f5f66582de2439081a03b9b3c15
murilo-scapim <mhscapim@gmail.com>
2024-09-04 21:11:22 -0300

    Método de cadastrar e validar entrada

--- src/br/com/fafram/view/CursoForm.java
+++ src/br/com/fafram/view/CursoForm.java
@@ -1,6 +1,11 @@
 package br.com.fafram.view;
 
+import br.com.fafram.dao.CursoDAOImpl;
+import br.com.fafram.model.Curso;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CursoForm {
     private JPanel panel;
@@ -11,4 +16,57 @@ public class CursoForm {
     private JButton atualizarButton;
     private JButton apagarButton;
     private JTable tableCursos;
+
+    public CursoForm() {
+        cadastrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cadastrar();
+            }
+        });
+    }
+
+    public void cadastrar() {
+        String nome = textFieldNome.getText();
+        String carga = textFieldCarga.getText();
+
+        if (validaEntrada(nome, carga)) {
+            Curso curso = new Curso();
+            curso.setNome(nome);
+            curso.setCargaHoraria(Integer.valueOf(carga));
+
+            CursoDAOImpl cursoDAO = new CursoDAOImpl();
+            cursoDAO.cadastrar(curso);
+        }
+    }
+
+    public boolean validaEntrada(String nome, String carga) {
+
+        if (nome.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nome não pode ser vazio!", "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        int cargaHoraria;
+        try {
+            cargaHoraria = Integer.parseInt(carga);
+            if (cargaHoraria <= 0) {
+                throw new NumberFormatException("A carga horária deve ser um número inteiro e positivo.");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Cadastro de curso");
+        frame.setContentPane(new CursoForm().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
